6792957906e45fea44c64f28b6e1ba89217b05da
Hugo Nicaise <38831847+Hnicaise@users.noreply.github.com>
2019-05-17 16:34:58 +0200

    Update

--- projects.php
+++ projects.php
@@ -5,42 +5,51 @@
         <?php $page = 'projects'; include 'nav.php' ?>
         <section class="container_page">
             <h2>My latest projects made with passion.</h2>
+            <br>
+            <br>
             <div class="row">
                 <div class="col2">
                     <article>
-                        <h3><img src="img/gastronaute.svg" alt="The gastronaute restaurant project">the gastronaute</h3>
+                        <h3><img src="img/gastronaute.svg" alt="The gastronaute restaurant project">THE GASTRONAUTE — Front-end developer</h3>
                         <p>One page website developed within the context of a school project. The gastronaute is molecular cuisine restaurant promoting his luxuary establishment.</p>
+                        <a href="#">→ read more</a>
                     </article>
-                    <br>
                     <article>
-                        <h3><img src="img/eye.svg" alt="Last trip movie clip project">last trip</h3>
+                        <h3><img src="img/eye.svg" alt="Last trip movie clip project">LAST TRIP — Director</h3>
                         <p>Movie clip that I produced for the brilliant french artist Idée Noire, in order to illustrate his music "Last Trip". My very first experience as director.</p>
+                        <a href="#">→ read more</a>
                     </article>
-                    <br>
                     <article>
-                        <h3><img src="img/brain.svg" alt="From 2060 game project">from 2060</h3>
+                        <h3><img src="img/brain.svg" alt="From 2060 game project">FROM 2060 — Lead developer & project manager</h3>
                         <p>Interactive online puzzle game in a neo-futurist world. To play the game you will need a printed game board, and also your brain.</p>
+                        <a href="#">→ read more</a>
                     </article>
-                    <br>
                     <article>
-                        <h3><img src="img/wrench.svg" alt="enr ouest project">enr ouest</h3>
+                        <h3><img src="img/wrench.svg" alt="ENR Ouest project">ENR OUEST — Front-end developer</h3>
                         <p>Website developed for a local plumbing and heating service company. One of my first professional website back in the days.</p>
+                        <a href="#">→ read more</a>
                     </article>
                 </div>
                 <div class="col2">
                     <article>
-                        <h3><img src="img/nailed.svg" alt="Nailed framework project">nailed</h3>
+                        <h3><img src="img/nailed.svg" alt="Nailed framework project">NAILED — Creator & web developer</h3>
                         <p>Powerful tool I created to assist front-end developers. Nailed is a framework based on flexbox that gives you a solid working basis.</p>
+                        <a href="#">→ read more</a>
                     </article>
-                    <br>
                     <article>
-                        <h3><img src="img/cards.svg" alt="Cybernative online card game project">cybernative</h3>
-                        <p>text.</p>
+                        <h3><img src="img/cards.svg" alt="Cybernative online card game project">CYBERNATIVE — Lead developer & project manager</h3>
+                        <p>Online card game playable either through any browser or real cards. Developed within the context of a school project.</p>
+                        <a href="#">→ read more</a>
                     </article>
-                    <br>
                     <article>
-                        <h3><img src="img/plane.svg" alt="Custom-air flight booking agency project">custom air</h3>
+                        <h3><img src="img/plane.svg" alt="Custom-air flight booking agency project">CUSTOM AIR — Front-end developer</h3>
                         <p>Fictive flight booking agency for which we created a complete mobile-friendly template within the context of a school project.</p>
+                        <a href="#">→ read more</a>
+                    </article>
+                    <article>
+                        <h3><img src="img/freelance.svg" alt="My previous freelancer website">MY PREVIOUS WEBSITE — Web developer</h3>
+                        <p>Because sometimes I want to show my improvements through the years. So I leave here my previous freelance contact platform.</p>
+                        <a href="#">→ read more</a>
                     </article>
                 </div>
             </div>
