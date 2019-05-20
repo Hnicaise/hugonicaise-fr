4489dc225c6bdf2a35ae35e0cbf2e8ae18330b08
Hugo Nicaise <38831847+Hnicaise@users.noreply.github.com>
2019-05-20 22:39:36 +0200

    Update

--- projects.php
+++ projects.php
@@ -10,54 +10,55 @@
                     <article class="more">
                         <h2><img src="img/nailed.svg" alt="Nailed framework project">NAILED — Creator & web developer</h2>
                         <p>Powerful tool I created to assist front-end developers. Nailed is a framework based on flexbox that gives you a solid working basis.</p>
-                        <a href="#">→ read more</a>
+                        <span class="read">→ read more</span>
                     </article>
                     <article class="more">
                         <h2><img src="img/eye.svg" alt="Last trip movie clip project">LAST TRIP — Director</h2>
                         <p>Movie clip that I produced for the brilliant french artist Idée Noire, in order to illustrate his music "Last Trip". My very first experience as director.</p>
-                        <a href="#">→ read more</a>
+                        <span class="read">→ read more</span>
                     </article>
                     <article class="more">
                         <h2><img src="img/gastronaute.svg" alt="The gastronaute restaurant project">THE GASTRONAUTE — Front-end developer</h2>
                         <p>One page website developed within the context of a school project. The gastronaute is molecular cuisine restaurant promoting his luxuary establishment.</p>
-                        <a href="#">→ read more</a>
+                        <span class="read">→ read more</span>
                     </article>
                     <article class="more">
                         <h2><img src="img/brain.svg" alt="From 2060 game project">FROM 2060 — Lead developer & project manager</h2>
                         <p>Interactive online puzzle game in a neo-futurist world. To play the game you will need a printed game board, and also your brain.</p>
-                        <a href="#">→ read more</a>
+                        <span class="read">→ read more</span>
                     </article>
                     <article class="more">
                         <h2><img src="img/wrench.svg" alt="ENR Ouest project">ENR OUEST — Front-end developer</h2>
                         <p>Website developed for a local plumbing and heating service company. One of my first professional website back in the days.</p>
-                        <a href="#">→ read more</a>
+                        <span class="read">→ read more</span>
                     </article>
                     <article class="more">
                         <h2><img src="img/cards.svg" alt="Cybernative online card game project">CYBERNATIVE — Lead developer & project manager</h2>
                         <p>Online card game playable either through any browser or real cards. Developed within the context of a school project.</p>
-                        <a href="#">→ read more</a>
+                        <span class="read">→ read more</span>
                     </article>
                     <article class="more">
                         <h2><img src="img/plane.svg" alt="Custom-air flight booking agency project">CUSTOM AIR — Front-end developer</h2>
                         <p>Fictive flight booking agency for which we created a complete mobile-friendly template within the context of a school project.</p>
-                        <a href="#">→ read more</a>
+                        <span class="read">→ read more</span>
                     </article>
                     <article class="more">
                         <h2><img src="img/freelance.svg" alt="My previous freelancer website">MY PREVIOUS WEBSITE — Web developer</h2>
                         <p>Because sometimes I want to show my improvements through the years. So I leave here my previous freelance contact platform.</p>
-                        <a href="#">→ read more</a>
+                        <span class="read">→ read more</span>
                     </article>
                     <article class="more">
                         <h2><img src="img/caravan.svg" alt="A-Pluriales website">A-PLURIALES — Front-end developer</h2>
                         <p>Website for a local camping-car shelter seller. Project developed at Zetruc Troyes agency during my internship.</p>
-                        <a href="#">→ read more</a>
+                        <span class="read">→ read more</span>
                     </article>
                     <article class="more">
                         <h2><img src="img/laptop.svg" alt="CL Portfolio website">CL Portfolio — Web developer</h2>
                         <p>Little but efficient one page website made for a friend that wanted to have a contact landing page. First real handmade website for someone.</p>
-                        <a href="#">→ read more</a>
+                        <span class="read">→ read more</span>
                     </article>
                 </div>
             </div>
         </section>
+        
         <?php include 'footer.php' ?>
