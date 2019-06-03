0b84d4de1fdaeb374fcd5087376ba8c976d259b4
Hugo Nicaise <38831847+Hnicaise@users.noreply.github.com>
2019-06-03 21:59:24 +0200

    Update

--- projects/cybernative.php
+++ projects/cybernative.php
@@ -2,32 +2,19 @@
     <h2>SUMMARY</h2>
     <div class="clapperboard">
         <div>
-            <h3>TITLE</h3>
-            <p>Cybernative</p>
+            <h3>WASTED PAPER</h3>
+            <p>999</p>
         </div>
         <div>
-            <h3>ROLE</h3>
-            <p>Project manager & front-end developer</p>
+            <h3>POWERPOINT SLIDES</h3>
+            <p>67</p>
         </div>
         <div>
-            <h3>DATE</h3>
-            <p>February - April 2019</p>
+            <h3>NATIVE KILLED</h3>
+            <p>0</p>
         </div>
     </div>
-    <h4>MOOD :<span>F*UCK GUYS! WE'RE LATE AGAIN!!!</span></h4>
+    <h4>Role :<span>Project manager & front-end developer</span></h4>
     <hr>
-    <h2>THE MISSION</h2>
-    <p class="col-2">Create a card game playable online or with real cards, based on the rules of an existing game : <i>"Ninja Taisen"</i>.</p>
-    <hr>
-    <h2>THE DELIVRABLES</h2>
-    <ol>
-        <li>A card game</li>
-        <li>A promotional website</li>
-        <li>An online game website</li>
-        <li>A teaser</li>
-        <li>An official trailer</li>
-        <li>A digital promotional campaign</li>
-        <li>Flyers</li>
-    </ol>
 </article>
 <br>
