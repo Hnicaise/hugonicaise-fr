020b65adb57d6fa41a7c541d6bf2fdf494c2cbe1
Hugo Nicaise <38831847+Hnicaise@users.noreply.github.com>
2019-06-04 23:50:48 +0200

    Up

--- projects/from2060.php
+++ projects/from2060.php
@@ -19,14 +19,17 @@
         <h4 class="role">Role :<span>Lead developer & project manager</span></h4>
         <h4 class="role">Date :<span>November 2018</span></h4>
         <h4 class="role">Team :<span><strong>Graphic designer:</strong> Anthony Inciarte, <strong>Print:</strong> Nathan Dhaynaut, <strong>Composer:</strong> Alexander Ceccaldi, <strong>Riddles:</strong> Jules Belluz, <strong>Web designer: </strong>Romane Ledoux, <strong>Back-end developer:</strong> Antonin Touron</span></h4>
-        <hr>
     </section>
     <section class="col-2">
         <h2>SYNOPSIS</h2>
-        <p>As a group of 6 students, I had to create a online puzzle game. The game had to be here to tell the story and guide you through the riddles. But to anwser them, you have to print a game board on which are displayed clues and missing instructions.</p>
+        <p>As a group of 6 students, we had to create a online puzzle game. The game had to be here to tell the story and guide you through the riddles. But to anwser them, you have to print a game board on which are displayed clues and missing instructions.</p>
         <br>
         <h2>DELIVRABLES</h2>
         <p>An online game, flyers, packagings, and a printed board.</p>
+    </section>
+    <section class="col-2"></section>
+    <section class="col-2">
+        <img src="img/mockup.png" alt="mockup printed game board" />
     </section>
 </article>
 <br>
