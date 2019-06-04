9ed6aff1341c299cb68924eca0d68e8459693d08
Hugo Nicaise <38831847+Hnicaise@users.noreply.github.com>
2019-06-04 11:38:04 +0200

    Update

--- projects/cybernative.php
+++ projects/cybernative.php
@@ -1,20 +1,27 @@
 <article class="report">
-    <h2>SUMMARY</h2>
-    <div class="clapperboard">
-        <div>
-            <h3>WASTED PAPER</h3>
-            <p>999</p>
+    <section class="col-2">
+        <h2>IN A NUTSHELL</h2>
+        <h4>Title :<span>Cybernative</span></h4>
+        <div class="clapperboard">
+            <div>
+                <h3>WASTED PAPER</h3>
+                <p>999</p>
+            </div>
+            <div>
+                <h3>POWERPOINT SLIDES</h3>
+                <p>67</p>
+            </div>
+            <div>
+                <h3>NATIVE KILLED</h3>
+                <p>0</p>
+            </div>
         </div>
-        <div>
-            <h3>POWERPOINT SLIDES</h3>
-            <p>67</p>
-        </div>
-        <div>
-            <h3>NATIVE KILLED</h3>
-            <p>0</p>
-        </div>
-    </div>
-    <h4>Role :<span>Project manager & front-end developer</span></h4>
-    <hr>
+        <h4 class="role">Role :<span>Project manager & front-end developer</span></h4>
+        <hr>
+    </section>
+    <section class="col-2">
+        <h2>SUMMARY</h2>
+        <p>Cybernative is a school project. We had to create a card game playable either with real cards or online through a browser. The game rules are inspired from an existing game: <i>"Ninja Taisen"</i>. Everything else has been created.</p>
+    </section>
 </article>
 <br>
