07b75fba360d1d8fb90b06078fe86f6655401d8a
Hugo Nicaise <38831847+Hnicaise@users.noreply.github.com>
2019-06-06 22:16:58 +0200

    Update

--- projects/last-trip.php
+++ projects/last-trip.php
@@ -19,14 +19,20 @@
         <h4 class="role">Role :<span>Director</span></h4>
         <h4 class="role">Date :<span>June 2019</span></h4>
         <h4 class="role">Team :<span><strong>Script:</strong> Alexander Ceccaldi (Idée Noire), <strong>Director of photography:</strong> Yannis Léger, <strong>Camera operator:</strong> Olivier Wysocinski, <strong>Film editor:</strong> David Marc, <strong>Stage manager: </strong>Jules Belluz</span></h4>
-        <hr>
     </section>
     <section class="col-2">
         <h2>SYNOPSIS</h2>
-        <p>For a school project as a team of 6 students we had to make a short movie without any imposed subject. <i>Idée noire</i>, a french artist but also a good friend, was part of our workgroup for this project. So, we decided to shot a video clip on his most famous track : <i>Last Trip</i>. We have taken up the challenge to shoot every shots at night, in one night.</p>
+        <p>For a school project as a team of 6six students we had to make a <strong>short movie</strong> without any imposed subject. <i>Idée noire</i>, a french artist but also a good friend, was part of our workgroup for this project. So, we decided to shot a <strong>video clip</strong> on his most famous track : <i>Last Trip</i>. We have taken up the challenge to shoot every shots at night, <strong>in one night</strong>.</p>
         <br>
         <h2>DELIVRABLES</h2>
         <p>A movie clip.</p>
+    </section>
+    <section class="col-2">
+        <hr>
+        <hr>
+        <h2>FILMING PREPARATION</h2>
+        <img src="img/last_artemis.jpg" />
+        <p class="caption">— Artemis Pro App</p>
     </section>
 </article>
 <br>
